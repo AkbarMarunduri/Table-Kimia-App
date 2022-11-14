95aac761e2265861d4aaee0c302bb23f03814403
Hiddemaru <72017596+hiddemaru@users.noreply.github.com>
2022-11-14 08:13:42 +0700

    share done

--- app/src/main/java/com/example/periodic_table_app/DetailActivity.kt
+++ app/src/main/java/com/example/periodic_table_app/DetailActivity.kt
@@ -1,10 +1,13 @@
 package com.example.periodic_table_app
 
+import android.content.ContentValues
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import android.view.Menu
@@ -17,6 +20,7 @@ import com.example.periodic_table_app.databinding.ActivityDetailBinding
 import com.example.periodic_table_app.model.Cell
 import kotlinx.android.synthetic.main.toolbar_main.*
 import java.io.File
+import java.io.OutputStream
 
 class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
@@ -43,18 +47,19 @@ class DetailActivity : AppCompatActivity() {
             .fitCenter()
             .into(binding.cellImageView)
         binding.apply {
-            tvStandardState.text=cell.StandardState
-            tvAtomicMass.text=cell.AtomicMass
-            tvElectronConfiguration.text=cell.ElectronConfiguration
-            tvOxidationStates.text=cell.OxidationStates
-            tvElectronegativity.text=cell.Electronegativity
-            tvAtomicRadiusls.text=cell.AtomicRadius
-            tvIonizationEnergy.text=cell.IonizationEnergy
-            tvElectronAffinity.text=cell.ElectronAffinity
-            tvMeltingPointy.text=cell.MeltingPoint
-            tvBoilingPoint.text=cell.BoilingPoint
-            tvDensity.text=cell.Density
-            tvYearDiscovered.text=cell.YearDiscovered
+            tvStandardState.text = cell.StandardState
+            tvAtomicMass.text = cell.AtomicMass
+            tvElectronConfiguration.text = cell.ElectronConfiguration
+            tvOxidationStates.text = cell.OxidationStates
+            tvElectronegativity.text = cell.Electronegativity
+            tvAtomicRadiusls.text = cell.AtomicRadius
+            tvIonizationEnergy.text = cell.IonizationEnergy
+            tvElectronAffinity.text = cell.ElectronAffinity
+            tvMeltingPointy.text = cell.MeltingPoint
+            tvBoilingPoint.text = cell.BoilingPoint
+            tvDensity.text = cell.Density
+            tvYearDiscovered.text = cell.YearDiscovered
+            tvGroupBlock.text = cell.GroupBlock
         }
     }
 
@@ -64,8 +69,7 @@ class DetailActivity : AppCompatActivity() {
     }
 
     fun onShareAction(item: MenuItem) {
-//        shareImage(getScreenShoot())
-        binding.cellImageView.setImageBitmap(getScreenShoot())
+        shareImage(getScreenShoot())
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -83,18 +87,41 @@ class DetailActivity : AppCompatActivity() {
     }
 
     fun shareImage(bitmap: Bitmap) {
-        val pathofbmp = MediaStore.Images.Media.insertImage(
-            applicationContext.contentResolver,
-            bitmap,
-            "title",
-            null
-        )
+        val pathofbmp = MediaStore.MediaColumns.IS_PENDING
+
         val shareIntent = Intent(Intent.ACTION_SEND)
         shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
         shareIntent.setType("image/*")
         shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Periodic App")
         shareIntent.putExtra(Intent.EXTRA_TEXT, "")
-        shareIntent.putExtra(Intent.EXTRA_STREAM, pathofbmp.toUri())
+        shareIntent.putExtra(Intent.EXTRA_STREAM, pathofbmp)
         startActivity(Intent.createChooser(shareIntent, "Hello"))
+
+//        val filenaame = "IMG_${System.currentTimeMillis()}.jpeg"
+//        var fos: OutputStream? = null
+//        var imageUri: Uri? = null
+//        val contentValues = ContentValues().apply {
+//            put(MediaStore.MediaColumns.DISPLAY_NAME, filenaame)
+//            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+//                put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+//                put(MediaStore.MediaColumns.IS_PENDING, 1)
+//            }
+//        }
+//
+//        val contentResolver = application.contentResolver
+//
+//        contentResolver.also { resolve ->
+//            imageUri = resolve.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+//            fos = imageUri?.let { resolve.openOutputStream(it) }
+//        }
+//
+//        fos?.use { bitmap.compress(Bitmap.CompressFormat.JPEG, 70, it) }
+//        contentValues.clear()
+//        contentValues.put(MediaStore.Video.Media.IS_PENDING, 1)
+//        contentResolver.update(imageUri!!, contentValues, null, null)
+//
+//        return imageUri!!
     }
+
 }
